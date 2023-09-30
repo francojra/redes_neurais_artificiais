d30e0ad8b123cfaa2c8f02873a749b0e6c78811a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-09-29 23:36:02 -0300

    Add files via upload

--- redes_neurais_artificiais.R
+++ redes_neurais_artificiais.R
@@ -7,4 +7,13 @@
 # Introdução -------------------------------------------------------------------------------------------------------------------------------
 
 ### Tenta emular matematicamente o funcionamento do cérebro humano.
+### Procura de maneira não linear padrões mais complexos dos dados nas
+### variáveis de entrada.
+### As redes neurais incluem os dados de entrada, as camadas intermediárias
+### que estão ligadas as camadas de entrada através do cálculo da função
+### de ativação (função que fornece a resposta da rede) que por sua vez
+### está ligada a função de saída.
+### A função de saída fornece a resposta da rede neural.
+### As redes neurais utilizam vários modelos complexos para melhor tentar
+### predizer a resposta.
 
